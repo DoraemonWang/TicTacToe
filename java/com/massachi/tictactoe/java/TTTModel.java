0ffd177ca3c56bd0364bc93dce999f70e47a941d
Sahar Massachi <sayhar@gmail.com>
2011-07-22 14:14:12 -0400

    retry

--- java/com/massachi/tictactoe/java/TTTModel.java
+++
@@ -1,8 +0,0 @@
-package com.massachi.tictactoe.java;
-
-public class TTTModel {
-
-	public static void main (String[] args){
-		System.out.println("sup");
-	}
-}
---
+++ java/com/saharmassachi/ttt/java/GamePlayer.java
@@ -0,0 +1,8 @@
+package com.saharmassachi.ttt.java;
+
+public class GamePlayer {
+
+	public static void main(String[] args){
+		System.out.println("hello");
+	}
+}
